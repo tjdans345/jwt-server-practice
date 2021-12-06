456297c628b6fdccf266e9cfedd679601a075c30
tjdans34568 <tjdans34568@gmail.com>
2021-12-06 09:00:36 +0900

    JwtAuthenticationFilter 커스텀
    attemptAuthentication 로그인처리
    -> successfulAuthentication
    성공 후 핸들러 로직 (jwt 생성) 추가 해야함
    20211204

--- src/main/java/com/meteor/jwt/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/meteor/jwt/jwt/JwtAuthenticationFilter.java
@@ -15,7 +15,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 
 // 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter 가 있음
